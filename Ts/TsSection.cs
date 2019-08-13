980c09e3438a5c5277fcc3938c7289d6f6d45ccc
Kay Diefenthal <Diefenthal@users.noreply.github.com>
2019-08-13 11:55:34 +0200

    Update TsSection.cs
    add last_section_number into Reset Method

--- Ts/TsSection.cs
+++ Ts/TsSection.cs
@@ -18,85 +18,86 @@ namespace SatIp
 {
     public class TsSection
     {
-        
-            public static int MAX_SECTION_LENGTH = 4300;
 
-            #region Public vars
-            public int table_id;
-            public int table_id_extension;
-            public int section_length;
-            public int section_number;
-            public int version_number;
-            public bool current_next_indicator;
-            public int section_syntax_indicator;
-            public int last_section_number;
+        public static int MAX_SECTION_LENGTH = 4300;
 
-            public int BufferPos;
-            public byte[] Data;
-            #endregion
+        #region Public vars
+        public int table_id;
+        public int table_id_extension;
+        public int section_length;
+        public int section_number;
+        public int version_number;
+        public bool current_next_indicator;
+        public int section_syntax_indicator;
+        public int last_section_number;
 
-            public TsSection()
+        public int BufferPos;
+        public byte[] Data;
+        #endregion
+
+        public TsSection()
+        {
+            Data = new byte[MAX_SECTION_LENGTH * 5];
+            Reset();
+        }
+
+        public void Reset()
+        {
+            table_id = -1;
+            table_id_extension = -1;
+            section_length = -1;
+            section_number = -1;
+            version_number = -1;
+            section_syntax_indicator = -1;
+            last_section_number = -1;
+            BufferPos = 0;
+            for (int i = 0; i < Data.Length; i++)
+                Data[i] = 0xFF;
+        }
+        public int CalcSectionLength(byte[] tsPacket, int start)
+        {
+            if (BufferPos < 3)
             {
-                Data = new byte[MAX_SECTION_LENGTH * 5];
-                Reset();
-            }
-
-            public void Reset()
-            {
-                table_id = -1;
-                table_id_extension = -1;
-                section_length = -1;
-                section_number = -1;
-                version_number = -1;
-                section_syntax_indicator = -1;
-                BufferPos = 0;
-                for (int i = 0; i < Data.Length; i++)
-                    Data[i] = 0xFF;
-            }
-            public int CalcSectionLength(byte[] tsPacket, int start)
-            {
-                if (BufferPos < 3)
+                byte bHi = 0;
+                byte bLow = 0;
+                if (BufferPos == 1)
                 {
-                    byte bHi = 0;
-                    byte bLow = 0;
-                    if (BufferPos == 1)
-                    {
-                        bHi = tsPacket[start];
-                        bLow = tsPacket[start + 1];
-                    }
-                    else if (BufferPos == 2)
-                    {
-                        bHi = Data[1];
-                        bLow = tsPacket[start];
-                    }
-                    section_length = (int)(((bHi & 0xF) << 8) + bLow);
+                    bHi = tsPacket[start];
+                    bLow = tsPacket[start + 1];
                 }
-                else
-                    section_length = (int)(((Data[1] & 0xF) << 8) + Data[2]);
-                return section_length;
+                else if (BufferPos == 2)
+                {
+                    bHi = Data[1];
+                    bLow = tsPacket[start];
+                }
+                section_length = (int)(((bHi & 0xF) << 8) + bLow);
             }
-            public bool DecodeHeader()
-            {
-                if (BufferPos < 8)
-                    return false;
-                table_id = (int)Data[0];
-                section_syntax_indicator = (int)((Data[1] >> 7) & 1);
-                if (section_length == -1)
-                    section_length = (int)(((Data[1] & 0xF) << 8) + Data[2]);
-                table_id_extension = ((Data[3] << 8) + Data[4]);
-                version_number = (int)((Data[5] >> 1) & 0x1F);
-                current_next_indicator = (Data[5] & 0x01) != 0;
-                section_number = (int)Data[6];
-                last_section_number = (int)Data[7];
+            else
+                section_length = (int)(((Data[1] & 0xF) << 8) + Data[2]);
+            return section_length;
+        }
+        public bool DecodeHeader()
+        {
+            if (BufferPos < 8)
+                return false;
+            table_id = (int)Data[0];
+            section_syntax_indicator = (int)((Data[1] >> 7) & 1);
+            if (section_length == -1)
+                section_length = (int)(((Data[1] & 0xF) << 8) + Data[2]);
+            table_id_extension = ((Data[3] << 8) + Data[4]);
+            version_number = (int)((Data[5] >> 1) & 0x1F);
+            current_next_indicator = (Data[5] & 0x01) != 0;
+            section_number = (int)Data[6];
+            last_section_number = (int)Data[7];
+            return true;
+        }
+        public bool SectionComplete()
+        {
+            if (!DecodeHeader() && BufferPos > section_length && section_length > 0)
                 return true;
-            }
-            public bool SectionComplete()
-            {
-                if (!DecodeHeader() && BufferPos > section_length && section_length > 0)
-                    return true;
-                if (!DecodeHeader())
-                    return false;
-                return (BufferPos >= section_length);
-            }
+            if (!DecodeHeader())
+                return false;
+            return (BufferPos >= section_length);
         }
     }
+}
